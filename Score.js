239fdbc673ec55ca66213000a9954ba8225e18d1
Adnan Aftab <adnanaftabxissc@gmail.com>
2024-09-08 22:18:54 +0530

    Importer Started

---
+++ Score.js
@@ -0,0 +1,9 @@
+const ele=document.querySelector("OneWord");
+ele.innerText="Hello";
+let s="Hi ";
+function Start(){
+    const ele=document.getElementById("ADDY");
+    ele.innerText=s;
+    s=s+"Adnan ";
+
+}
