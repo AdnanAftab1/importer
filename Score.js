15367b4ed19a948c069879638a83a6451c463559
Adnan Aftab <adnanaftabxissc@gmail.com>
2024-09-08 23:27:30 +0530

    GITS

--- Score.js
+++ Score.js
@@ -1,9 +1,13 @@
-const ele=document.querySelector("OneWord");
-ele.innerText="Hello";
+import {list} from "./main.js";
+const ele1=document.querySelector(".OneWord");
+console.log(ele1);
 let s="Hi ";
+ele1.innerText="Hello";
+Start();
 function Start(){
     const ele=document.getElementById("ADDY");
-    ele.innerText=s;
-    s=s+"Adnan ";
+    for (let index = 0; index < 6; index++) {
+        s=s+`   ${list[index]}`;
+    }
 
 }
